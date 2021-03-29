f1c0fb4786e09a74ec1bb242f2d150d1a0ae6423
Cl Sayart <clsayart@gmail.com>
2021-03-29 12:07:48 +0200

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,41 @@ class Home extends React.Component {
 
     this.state = {
       // allContacts: contactsFromJSON,
-      artworks: []
+      gallery: []
     };
+  }
+
+  componentDidMount() {
+    // this.getAll();
+    axios
+      .get("http://localhost:4000/api/artworks")
+      .then((response) => {
+        this.setState({ gallery: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  addRandomContact() {
+    // console.log("random clicked"); Always look if  you event handler works before any logic ;)
+    let randomIndex = Math.floor(contactsFromJSON.length * Math.random()); // random number between 0 and contactsFromJSON.length (excluded)
+    let randomContact = contactsFromJSON[randomIndex];
+
+    // Method 1
+    // Create a copy of this.state.firstVisibleContacts
+    let newList = [...this.state.firstVisibleContacts];
+    // push or unshift in the copy you just created
+    newList.unshift(randomContact);
+    // save it in the state
+    this.setState({
+      firstVisibleContacts: newList,
+    });
+
+    // Method 2
+    // this.setState({
+    //   firstVisibleContacts: [...this.state.firstVisibleContacts, randomContact]
+    // })
   }
 
   render() {
@@ -17,9 +50,15 @@ class Home extends React.Component {
         <h1>Home Page ∆</h1>
        
         <div>ARTWORKS</div>
-      </div>
+        <div>{this.state.gallery.map((oneArtPiece) => {
 
 
+            return (
+              <div>
+                <p>{oneArtPiece.pictureUrl}</p></div>
+         ) }) } </div>
+
+       </div>
     );
   }
 }
