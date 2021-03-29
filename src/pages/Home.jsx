658dcbf8b5cbc1d3f01355418632d462facde1dc
Paul Parant <paul.parant@gmail.com>
2021-03-29 12:49:11 +0200

    trying to randomize

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       // allContacts: contactsFromJSON,
-      gallery: []
+      gallery: [],
     };
   }
 
@@ -17,48 +18,71 @@ class Home extends React.Component {
       .get("http://localhost:4000/api/artworks")
       .then((response) => {
         this.setState({ gallery: response.data });
+        console.log("THIS.STATE.GALLERY:");
+        console.log(this.state.gallery);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
-  addRandomContact() {
-    // console.log("random clicked"); Always look if  you event handler works before any logic ;)
-    let randomIndex = Math.floor(contactsFromJSON.length * Math.random()); // random number between 0 and contactsFromJSON.length (excluded)
-    let randomContact = contactsFromJSON[randomIndex];
+  // addRandomContact() {
+  //   // console.log("random clicked"); Always look if  you event handler works before any logic ;)
+  //   let randomIndex = Math.floor(contactsFromJSON.length * Math.random()); // random number between 0 and contactsFromJSON.length (excluded)
+  //   let randomContact = contactsFromJSON[randomIndex];
 
-    // Method 1
-    // Create a copy of this.state.firstVisibleContacts
-    let newList = [...this.state.firstVisibleContacts];
-    // push or unshift in the copy you just created
-    newList.unshift(randomContact);
-    // save it in the state
-    this.setState({
-      firstVisibleContacts: newList,
-    });
+  //   // Method 1
+  //   // Create a copy of this.state.firstVisibleContacts
+  //   let newList = [...this.state.firstVisibleContacts];
+  //   // push or unshift in the copy you just created
+  //   newList.unshift(randomContact);
+  //   // save it in the state
+  //   this.setState({
+  //     firstVisibleContacts: newList,
+  //   });
 
-    // Method 2
-    // this.setState({
-    //   firstVisibleContacts: [...this.state.firstVisibleContacts, randomContact]
-    // })
+  //   // Method 2
+  //   // this.setState({
+  //   //   firstVisibleContacts: [...this.state.firstVisibleContacts, randomContact]
+  //   // })
+  // }
+
+
+  randomizeArtwork(array) {
+    let randomIndex = Math.floor(array.length * Math.random());
+    let randomArtwork = array[randomIndex];
+    return randomArtwork;
+    // console.log("RANDOMARTWORK");
+    // console.log(randomArtwork);
   }
 
   render() {
     return (
       <div>
         <h1>Home Page ∆</h1>
-       
-        <div>ARTWORKS</div>
-        <div>{this.state.gallery.map((oneArtPiece) => {
+
+        <div>RANDOM ARTWORK</div>
+        <div>
+          {
+          randomizeArtwork(this.state.gallery);
+          }
+          
+        {/* <img src={randomArtwork.pictureUrl} alt={randomArtwork.title} /> */}
+        
 
 
+
+          {  this.state.gallery.map((oneArtPiece) => {
             return (
               <div>
-                <p>{oneArtPiece.pictureUrl}</p></div>
-         ) }) } </div>
-
-       </div>
+                <p>
+                  <img src={oneArtPiece.pictureUrl} alt={oneArtPiece.title} />
+                </p>
+              </div>
+            );
+          })}{" "}
+        </div>
+      </div>
     );
   }
 }
